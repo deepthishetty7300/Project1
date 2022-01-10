7a95f6c389cc0913b234886c0fbdc9abe66810fb
niveditharai <niveditharaih@gmail.com>
2022-01-10 17:19:06 +0530

    convert int to string

--- main.go
+++ main.go
@@ -2,38 +2,19 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"strconv"
 )
 
 func main() {
-	var s []int
-	var input int
-	fmt.Print("enter the number\n")
-	fmt.Scanln(&input)
-	t := reflect.ValueOf(input).Kind()
-	fmt.Println(t)
-	var sum int = 0
-	for input > 0 {
-		sum += input % 10
-		s = append(s, input%10)
-		input /= 10
+	conv(123)
+}
 
+func conv(num int) {
+	var s string = ""
+
+	for num > 0 {
+		s = strconv.Itoa(num%10) + s
+		num = num / 10
 	}
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, 0 {
-		s[i], s[j] = s[j], s[i]
-	}
-	fmt.Println(s)
-	if sum > 9 {
-		var total int = 0
-		for sum > 0 {
-			total += sum % 10
-			sum /= 10
-
-		}
-		fmt.Println("sum in unidigit", total)
-
-	} else {
-		fmt.Println("sum in unidigit", sum)
-	}
-
+	fmt.Printf("%[1]T, %[1]s\n", s)
 }
