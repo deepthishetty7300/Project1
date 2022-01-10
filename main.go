2fb32b4a06191438ce979549613e7f2a2b472895
niveditharai <niveditharaih@gmail.com>
2022-01-10 12:25:47 +0530

    storing input into slice

--- main.go
+++ main.go
@@ -2,31 +2,38 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func main() {
+	var s []int
 	var input int
 	fmt.Print("enter the number\n")
 	fmt.Scanln(&input)
-
+	t := reflect.ValueOf(input).Kind()
+	fmt.Println(t)
 	var sum int = 0
 	for input > 0 {
 		sum += input % 10
+		s = append(s, input%10)
 		input /= 10
 
 	}
-
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, 0 {
+		s[i], s[j] = s[j], s[i]
+	}
+	fmt.Println(s)
 	if sum > 9 {
 		var total int = 0
 		for sum > 0 {
-			total = total + sum%10
-			sum = sum / 10
+			total += sum % 10
+			sum /= 10
 
 		}
-		fmt.Println(total)
+		fmt.Println("sum in unidigit", total)
 
 	} else {
-		fmt.Println(sum)
+		fmt.Println("sum in unidigit", sum)
 	}
 
 }
